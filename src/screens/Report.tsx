5565620a57a696149af069479429ab14ec62705c
SB2318 <sb2318@it.jgec.ac.in>
2025-06-12 12:13:08 +0530

    --report--admin--frontend-work--start

--- src/screens/Report.tsx
+++ src/screens/Report.tsx
@@ -1,27 +1,185 @@
-import { View, Text, StyleSheet } from "react-native";
-import { ReportScreenProps } from "../type";
-import { ON_PRIMARY_COLOR } from "../helper/Theme";
+import {View, Text, StyleSheet} from 'react-native';
+import {BUTTON_COLOR, ON_PRIMARY_COLOR} from '../helper/Theme';
+import {useRef} from 'react';
+import {Tabs, MaterialTabBar} from 'react-native-collapsible-tab-view';
+import {useSelector} from 'react-redux';
+import {useCallback} from 'react';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {hp} from '../helper/Metric';
+import {BottomSheetModal} from '@gorhom/bottom-sheet';
 
+export default function HomeScreen() {
+  const {user_id} = useSelector((state: any) => state.user);
+ 
 
-export default function Report({navigation}:ReportScreenProps){
-    return(
-        <View style={styles.container}>
-            <Text style={styles.text}>Report</Text>
-        </View>
-    )
+  //const bottomBarHeight = useBottomTabBarHeight();
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const insets = useSafeAreaInsets();
+
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+
+  const renderTabBar = props => {
+    return (
+      <MaterialTabBar
+        {...props}
+        indicatorStyle={styles.indicatorStyle}
+        style={styles.tabBarStyle}
+        activeColor={BUTTON_COLOR}
+        inactiveColor="#9098A3"
+        labelStyle={styles.labelStyle}
+        contentContainerStyle={styles.contentContainerStyle}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={[styles.innerContainer, {paddingTop: insets.top}]}>
+     
+        <Tabs.Container
+          //initialIndex={0}
+          renderTabBar={renderTabBar}
+          containerStyle={styles.tabsContainer}>
+          <Tabs.Tab name="Reasons">
+            <Tabs.ScrollView contentContainerStyle={styles.nocontainer}>
+              <Text style={styles.text}>Reasons</Text>
+            </Tabs.ScrollView>
+          </Tabs.Tab>
+
+          <Tabs.Tab name="Pending">
+            <Tabs.ScrollView contentContainerStyle={styles.nocontainer}>
+              <Text style={styles.text}>Pending Report</Text>
+            </Tabs.ScrollView>
+          </Tabs.Tab>
+
+          <Tabs.Tab name="Assigned">
+            <Tabs.ScrollView contentContainerStyle={styles.nocontainer}>
+              <Text style={styles.text}>Assigned Report</Text>
+            </Tabs.ScrollView>
+          </Tabs.Tab>
+        </Tabs.Container>
+
+       {
+        /**
+         * 
+         *  <FilterModal
+          bottomSheetModalRef={bottomSheetModalRef}
+          categories={articleCategories}
+          handleCategorySelection={handleCategorySelection}
+          selectCategoryList={selectCategoryList}
+          handleFilterReset={handleFilterReset}
+          handleFilterApply={handleFilterApply}
+          setSortingType={setSortingType}
+          sortingType={sortingType}
+        />
+        <FAB
+          style={styles.fab}
+          small
+          icon={() => <AntDesign color="white" name="menu-fold" size={25} />}
+          onPress={() => {
+            //navigation.goBack();
+            handlePresentModalPress();
+          }}
+        />
+         */
+       }
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0CAFFF',
+  },
+  innerContainer: {
+    flex: 1,
+  },
+  tabsContainer: {
+    backgroundColor: 'white',
+    overflow: 'hidden',
+  },
+  image: {
+    height: 160,
+    width: 160,
+    borderRadius: 80,
+    resizeMode: 'cover',
+    marginBottom: hp(4),
+  },
+  scrollViewContentContainer: {
+    paddingHorizontal: 16,
+    marginTop: 16,
+    backgroundColor: ON_PRIMARY_COLOR,
+  },
+  flatListContentContainer: {
+    paddingHorizontal: 16,
+    backgroundColor: ON_PRIMARY_COLOR,
+  },
 
-     container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: ON_PRIMARY_COLOR
-     },
-     text:{
-        fontSize: 20,
-        color: '#000',
-     }
+  profileImage: {
+    height: 130,
+    width: 130,
+    borderRadius: 100,
+    objectFit: 'cover',
+    resizeMode: 'contain',
+  },
+  indicatorStyle: {
+    backgroundColor: 'white',
+  },
+  tabBarStyle: {
+    backgroundColor: 'white',
+  },
+  labelStyle: {
+    fontWeight: '600',
+    fontSize: 17,
+    color: 'black',
+    textTransform: 'capitalize',
+  },
+  contentContainerStyle: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    shadowOpacity: 0,
+    shadowOffset: {width: 0, height: 0},
+    shadowColor: 'white',
+  },
+  message: {
+    fontSize: 17,
+    color: '#555',
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fab: {
+    position: 'absolute',
+    right: 20,
+    bottom: 60,
+    borderRadius: hp(20),
+    backgroundColor: BUTTON_COLOR,
+  },
 
+  nocontainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: ON_PRIMARY_COLOR,
+  },
+  text: {
+    fontSize: 20,
+    color: '#000',
+  },
 });
